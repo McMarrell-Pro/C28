371e663c81cc73c4c1e81827bd54993be1a75d58
McMarrell-Pro <64479501+McMarrell-Pro@users.noreply.github.com>
2021-01-06 23:32:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var tree, boy, boyImg,stone,mango1,mango2,mango3;
+
+function preload()
+{
+ boyImg = loadImage("Plucking mangoes/boy.png");
+
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	boy = createSprite(200,600,50,50);
+	boy.addImage(boyImg);
+	boy.scale = 0.15;
+
+	tree = new Tree(700,600,50,60);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  tree.display();
+  
+  drawSprites();
+ 
+}
+
+
+
---
+++ tree.js
@@ -0,0 +1,22 @@
+class Tree{
+constructor(x,y,width,height){
+var options = {
+isStatic:false,
+restitution:0,
+friction:1,
+density:1.2
+
+}
+this.body = Bodies.rectangle(x, y, width, height, options);
+this.width = width;
+this.height = height;
+this.image = loadImage("Plucking mangoes/boy.png");
+World.add(world, this.body);
+
+}
+display(){
+imageMode(CENTER);
+image(this.image,0, 0, this.width, this.height);
+}
+
+}
